193d86c969b8d1e5ddf1f2d031e0b7cacd19adac
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2023-04-13 06:47:57 +0200

    Do not verify ssl

--- Source/Api.php
+++ Source/Api.php
@@ -5,6 +5,7 @@ namespace PhpRepos\ChuckNorris\Api;
 function get(string $url): array
 {
     $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($ch);
     curl_close($ch);
